39faeeac1f721a2f254496e29aacf33f4d18724c
jzf21 <jozefneelamkavil2003@gmail.com>
2023-11-11 20:11:10 +0530

    add env

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const server = require("http").createServer(app);
 const wss = new WebSocket.Server({ server });
 const path = require("path");
-process.env.GOOGLE_APPLICATION_CREDENTIALS = 'D:/Harin/Projects/Customate/customate-404808-746ea5c70c80.json';
+process.env.GOOGLE_APPLICATION_CREDENTIALS = process.env.PATH;
 let transcription = "";
 //Include Google Speech to Text
 const speech = require("@google-cloud/speech");
@@ -15,56 +15,53 @@ const request = {
   config: {
     encoding: "MULAW",
     sampleRateHertz: 8000,
-    languageCode: "en-GB"
+    languageCode: "en-GB",
   },
-  interimResults: true
+  interimResults: true,
 };
 // Handle Web Socket Connection
 wss.on("connection", function connection(ws) {
-    console.log("New Connection Initiated");
-    
-    let recognizeStream = null;
-    
-       ws.on("message", function incoming(message) {
-        const msg = JSON.parse(message);
-        switch (msg.event) {
-          case "connected":
-            console.log(`A new call has connected.`);
-      //Create Stream to the Google Speech to Text API
-      recognizeStream = client
-        .streamingRecognize(request)
-        .on("error", console.error)
-        .on("data", data => {
-        
-          console.log(data.results[0].alternatives[0].transcript);
-          transcription = data.results[0].alternatives[0].transcript;
-          wss.clients.forEach( client => {
-               if (client.readyState === WebSocket.OPEN) {
-                 client.send(
-                   JSON.stringify({
-                   event: "interim-transcription",
-                   text: data.results[0].alternatives[0].transcript
-                 })
-               );
-             }
-           });
-    
-        });
-    
-            break;
-          case "start":
-            console.log(`Starting Media Stream ${msg.streamSid}`);
-            break;
-          case "media":
-            // Write Media Packets to the recognize stream
-            recognizeStream.write(msg.media.payload);
-            break;
-          case "stop":
-            console.log(`Call Has Ended`);
-            recognizeStream.destroy();
-            console.log(transcription);
-            break;
-        }
-      });
-    
-    });
+  console.log("New Connection Initiated");
+
+  let recognizeStream = null;
+
+  ws.on("message", function incoming(message) {
+    const msg = JSON.parse(message);
+    switch (msg.event) {
+      case "connected":
+        console.log(`A new call has connected.`);
+        //Create Stream to the Google Speech to Text API
+        recognizeStream = client
+          .streamingRecognize(request)
+          .on("error", console.error)
+          .on("data", (data) => {
+            console.log(data.results[0].alternatives[0].transcript);
+            transcription = data.results[0].alternatives[0].transcript;
+            wss.clients.forEach((client) => {
+              if (client.readyState === WebSocket.OPEN) {
+                client.send(
+                  JSON.stringify({
+                    event: "interim-transcription",
+                    text: data.results[0].alternatives[0].transcript,
+                  })
+                );
+              }
+            });
+          });
+
+        break;
+      case "start":
+        console.log(`Starting Media Stream ${msg.streamSid}`);
+        break;
+      case "media":
+        // Write Media Packets to the recognize stream
+        recognizeStream.write(msg.media.payload);
+        break;
+      case "stop":
+        console.log(`Call Has Ended`);
+        recognizeStream.destroy();
+        console.log(transcription);
+        break;
+    }
+  });
+});

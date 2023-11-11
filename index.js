dd3fd8a572be4660add5b95e6d73779276ab00dc
ZeroDeath2 <harinchirappannath@gmail.com>
2023-11-11 19:22:06 +0530

    Added websocket to twilio account

--- index.js
+++ index.js
@@ -19,4 +19,52 @@ const request = {
   },
   interimResults: true
 };
-
+// Handle Web Socket Connection
+wss.on("connection", function connection(ws) {
+    console.log("New Connection Initiated");
+    
+    let recognizeStream = null;
+    
+       ws.on("message", function incoming(message) {
+        const msg = JSON.parse(message);
+        switch (msg.event) {
+          case "connected":
+            console.log(`A new call has connected.`);
+      //Create Stream to the Google Speech to Text API
+      recognizeStream = client
+        .streamingRecognize(request)
+        .on("error", console.error)
+        .on("data", data => {
+        
+          console.log(data.results[0].alternatives[0].transcript);
+          transcription = data.results[0].alternatives[0].transcript;
+          wss.clients.forEach( client => {
+               if (client.readyState === WebSocket.OPEN) {
+                 client.send(
+                   JSON.stringify({
+                   event: "interim-transcription",
+                   text: data.results[0].alternatives[0].transcript
+                 })
+               );
+             }
+           });
+    
+        });
+    
+            break;
+          case "start":
+            console.log(`Starting Media Stream ${msg.streamSid}`);
+            break;
+          case "media":
+            // Write Media Packets to the recognize stream
+            recognizeStream.write(msg.media.payload);
+            break;
+          case "stop":
+            console.log(`Call Has Ended`);
+            recognizeStream.destroy();
+            console.log(transcription);
+            break;
+        }
+      });
+    
+    });

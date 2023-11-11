a30e7394c738be7c1df68a86c8f7cec41c7a80f8
ZeroDeath2 <harinchirappannath@gmail.com>
2023-11-11 19:00:00 +0530

    Added google speech to text client

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ const wss = new WebSocket.Server({ server });
 const path = require("path");
 process.env.GOOGLE_APPLICATION_CREDENTIALS = 'D:/Harin/Projects/Customate/customate-404808-746ea5c70c80.json';
 let transcription = "";
+//Include Google Speech to Text
+const speech = require("@google-cloud/speech");
+const client = new speech.SpeechClient();
+
+//Configure Transcription Request
+const request = {
+  config: {
+    encoding: "MULAW",
+    sampleRateHertz: 8000,
+    languageCode: "en-GB"
+  },
+  interimResults: true
+};
+
